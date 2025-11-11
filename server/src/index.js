2bd0901bd8d1c7bdfc8166b331933b9479df7025
mdcode-00 <consolcode625@gmail.com>
2025-11-11 15:01:06 +0530

    mobgoDB Clusters & mongoose setUp

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import notesRoutes from './routes/notes.routes.js';
+import connectDB from './config/db.js';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+connectDB();
 
 app.use('/api/notes', notesRoutes);
 
