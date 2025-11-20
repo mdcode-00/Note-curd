29d5186a3c0d7d63e51b06f5654a57108d8225cb
mdcode-00 <consolcode625@gmail.com>
2025-11-20 15:49:37 +0530

    some-change

--- api/index.js
+++
@@ -1,5 +0,0 @@
-import app from "../server/src/index.js";  // your Express app
-
-export default function handler(req, res) {
-  return app(req, res);
-}
--- clint/src/api.js
+++ clint/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'https://note-curd.vercel.app/api'
+const API_BASE = import.meta.env.VITE_API_URL
 
 export async function fetchNotes() {
   const res = await fetch(`${API_BASE}/notes`);
