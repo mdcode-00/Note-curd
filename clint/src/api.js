db173a42706d57a939aa9fed6e04760523d7cdb1
mdcode-00 <consolcode625@gmail.com>
2025-11-20 16:19:04 +0530

    some-change

--- clint/src/api.js
+++ clint/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_URL
+const API_BASE = 'https://note-curd.vercel.app/api'
 
 export async function fetchNotes() {
   const res = await fetch(`${API_BASE}/notes`);
