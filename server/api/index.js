effc1962467f386893ebf7cbf79d5f148e7830bf
mdcode-00 <consolcode625@gmail.com>
2025-11-20 13:42:36 +0530

    fix-9

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,5 @@
-import serverless from 'serverless-http';
-import app from '../src/index.js';
+import app from "../src/index.js";  // your Express app
 
-export default serverless(app);
+export default function handler(req, res) {
+  return app(req, res);
+}
