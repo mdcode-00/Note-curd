1fc0cde6a79fd2b8e028c159f9dd6c854c47ad87
mdcode-00 <consolcode625@gmail.com>
2025-11-20 14:25:26 +0530

    fix-11

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-import app from "../src/index.js";  // your Express app
+import app from "../server/src/index.js";  // your Express app
 
 export default function handler(req, res) {
   return app(req, res);
